a634dc73fa27edc6efe4bdce7ecc6255acaac27d
plaze <banikakash131@gamil.com>
2023-04-13 16:05:20 +0530

    domFunction.js added and modified

--- dist/script/main.js
+++ dist/script/main.js
@@ -1,6 +1,25 @@
+import{
+    addSpinner
+} from "./domFunction.js";
+import CurrentLocation from "./currentLocation.js";
+const currentLoc = new CurrentLocation();
+
 const initApp = ()=>{
     //add event listener
+    const geoButton = document.getElementById("getLocation");
+    geoButton.addEventListener("click",getGeoWeather);
     //setup
     //load weather
 }
 document.addEventListener("DOMContentLoaded",initApp);
+
+const getGeoWeather = (event) =>{
+    if(event){
+        if(event.type == "click"){
+            const mapIcon = document.querySelector(".fa-map-marker-alt");
+            addSpinner(mapIcon);
+        }
+    }
+    // if(!navigator.geolocation) geoError();
+    // navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
