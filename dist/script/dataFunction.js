db5f986bfd2db4be9186dc6bcbd18728148fa5f6
plaze <banikakash131@gamil.com>
2023-04-15 22:15:38 +0530

    api fetch added

--- dist/script/dataFunction.js
+++ dist/script/dataFunction.js
@@ -17,7 +17,7 @@ export const getHomeLocation = ()=>{
 export const getCoordsFromApi = async (entryText, units) =>{
     const regex = /^\d+$/g;
     const flag = regex.test(entryText)?"zip":"q";
-    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&APPID=${WEATHER_API_KEY}`;
     const encodedUrl = encodeURI(url);
     try{
         const dataStream = await fetch(encodedUrl);
